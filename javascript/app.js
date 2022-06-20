046945420f542c4e3a77ed2c2050d996e5aa5073
Eliel-Carvalho <107527047+Eliel-Carvalho@users.noreply.github.com>
2022-06-19 23:19:53 -0300

    tornando formulario de cadastro interativo

--- javascript/app.js
+++ javascript/app.js
@@ -1,6 +1,28 @@
-const btnSubmit = document.getElementById('but')
-const conteiner =  document.querySelector('div.conteiner')
+let submit = document.getElementById('submit')
+let firstname = document.getElementById('firstname')
+let lastname = document.getElementById('lastname')
+let email = document.getElementById('email')
+let number = document.getElementById('number')
+let password = document.getElementById('password')
+let passwordConfirmation = document.getElementById('password-confirmation')
 
-btnSubmit.addEventListener('click', (Event) =>{
+let radSex = document.getElementsByName('gender')
 
+submit.addEventListener('click', function(){
+    eventIput()
 })
+
+function eventIput(){
+    
+    if(firstname.value.length == 0 || lastname.value.length == 0 || email.value.length == 0 || number.value.length == 0 || password.value.length == 0){
+
+        firstname.classList.add('show')
+        lastname.classList.add('show')
+        email.classList.add('show')
+        number.classList.add('show')
+        password.classList.add('show')
+        passwordConfirmation.classList.add('show')
+    }
+}
+
+
