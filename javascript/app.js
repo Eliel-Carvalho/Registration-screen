15b3e793bab65acea9adbb93bf9e71f930055d82
Eliel-Carvalho <107527047+Eliel-Carvalho@users.noreply.github.com>
2022-06-20 23:48:58 -0300

    Refatorando codigo Javascript

--- javascript/app.js
+++ javascript/app.js
@@ -5,24 +5,10 @@ let email = document.getElementById('email')
 let number = document.getElementById('number')
 let password = document.getElementById('password')
 let passwordConfirmation = document.getElementById('password-confirmation')
-
 let radSex = document.getElementsByName('gender')
 
 submit.addEventListener('click', function(){
-    eventIput()
-})
-
-function eventIput(){
     
-    if(firstname.value.length == 0 || lastname.value.length == 0 || email.value.length == 0 || number.value.length == 0 || password.value.length == 0){
-
-        firstname.classList.add('show')
-        lastname.classList.add('show')
-        email.classList.add('show')
-        number.classList.add('show')
-        password.classList.add('show')
-        passwordConfirmation.classList.add('show')
-    }
-}
+})
 
 
