9eb591cd487a177f7a9a4dc2742f7c98710a1ffb
Eliel-Carvalho <107527047+Eliel-Carvalho@users.noreply.github.com>
2022-06-21 21:34:35 -0300

    Codigo Javascript não roda

--- javascript/app.js
+++ javascript/app.js
@@ -8,7 +8,38 @@ let passwordConfirmation = document.getElementById('password-confirmation')
 let radSex = document.getElementsByName('gender')
 
 submit.addEventListener('click', function(){
-    
+    checkInputs()
 })
+
+function checkInputs() {
+    let firstnameValue = firstname.value 
+    let lastnameValue = lastname.value 
+    let emailValue = email.value 
+    let numberValue = number.value 
+    let passwordValue = password.value 
+    let passworConfimationdValue = passwordConfirmation.value 
+
+    if(firstnameValue === '') {
+        setErroFor(firstname, 'O nome do usuário é obrigatorio.')
+    }
+}
+
+function setErroFor(input, message) {
+    let inputBox = input.parentElement
+    let  small = inputBox.querySelector('smail')
+
+    //messagem de erro
+    small.innerText = 'message'
+
+    //adicionar a class de erro
+    inputBox.className = 'input-box error'
+}
+
+function setErroFor() {
+    let inputBox = input.parentElement//retorna a div pai do input (input-box)
+
+    //adcionar a class de success
+    inputBox.className = 'input-box success'
+}
 
 
