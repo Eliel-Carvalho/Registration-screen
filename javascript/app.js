467e4a44c06c79714be24b6e70348d70ce53848b
Eliel-Carvalho <107527047+Eliel-Carvalho@users.noreply.github.com>
2022-06-26 15:45:12 -0300

    Validação dos campos incompleta, function regex de validação de numero de telefone,com bug

--- javascript/app.js
+++ javascript/app.js
@@ -8,38 +8,66 @@ let passwordConfirmation = document.getElementById('password-confirmation')
 let radSex = document.getElementsByName('gender')
 
 submit.addEventListener('click', function(){
-    checkInputs()
+    setErroFor()
 })
 
-function checkInputs() {
-    let firstnameValue = firstname.value 
-    let lastnameValue = lastname.value 
-    let emailValue = email.value 
-    let numberValue = number.value 
-    let passwordValue = password.value 
-    let passworConfimationdValue = passwordConfirmation.value 
-
-    if(firstnameValue === '') {
-        setErroFor(firstname, 'O nome do usuário é obrigatorio.')
+function setErroFor(){
+    if(firstname.value.length === 0){
+        firstname.classList.add('erro')
+        alert('Preencha os Campos')
+    }else{
+        firstname.classList.add('success')
+    } 
+    
+    if(lastname.value.length === 0){
+        lastname.classList.add('erro')  
+    }else{
+        lastname.classList.add('success')
     }
+
+    if(email.value.length === 0){
+        email.classList.add('erro')
+    }else if(!validateEmail (email.value)){
+        alert('Insira um Email Validor')
+    }else{
+        email.classList.add('success')
+    }
+
+    if(number.value.length === 0){
+        number.classList.add('erro')
+    }else if(!validPhone (number.value)){
+        number.classList.add('erro')
+        alert('Numero de teleforne Invalido!')
+    }else{
+        number.classList.add('success')
+    }
+
+    if(password.value.length === 0){
+        password.classList.add('erro') 
+    }else if(password.value.length < 4){
+        alert('Sua senha não pode ter menos que 4 caracteres')
+        password.classList.add('erro') 
+    }else{
+        password.classList.add('success') 
+    }
+
+    if(passwordConfirmation.value.length === 0){
+        passwordConfirmation.classList.add('erro') 
+    }else if(passwordConfirmation !== password){
+        passwordConfirmation.classList.add('erro') 
+        alert('ERRO DE COMFIRMAÇÃO') 
+    }else{
+        passwordConfirmation.classList.add('success') 
+    }
+    
 }
 
-function setErroFor(input, message) {
-    let inputBox = input.parentElement
-    let  small = inputBox.querySelector('smail')
-
-    //messagem de erro
-    small.innerText = 'message'
-
-    //adicionar a class de erro
-    inputBox.className = 'input-box error'
+function validPhone (phone) {
+    var regex = new RegExp('^\\([0-9]{2}\\)((3[0-9]{3}-[0-9]{4})|(9[0-9]{3}-[0-9]{5}))$');
+    return regex.test(phone);
 }
 
-function setErroFor() {
-    let inputBox = input.parentElement//retorna a div pai do input (input-box)
-
-    //adcionar a class de success
-    inputBox.className = 'input-box success'
-}
-
-
+function validateEmail(email) {
+    var re = /\S+@\S+\.\S+/;
+    return re.test(email);
+  }
